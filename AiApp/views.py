4eadc648ae107968f846fdf5c39164fd49e7c6a7
Shubham <byshubham6@gmail.com>
2024-05-23 17:30:38 +0530

    Add file

--- AiApp/views.py
+++ AiApp/views.py
@@ -1,3 +1,20 @@
+# main/views.py
 from django.shortcuts import render
+from django.http import JsonResponse
+import openai
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def generate_text(request):
+    if request.method == 'POST':
+        prompt = request.POST.get('prompt')
+        response = openai.Completion.create(
+            engine="davinci",
+            prompt=prompt,
+            max_tokens=100
+        )
+        return JsonResponse({'text': response.choices[0].text})
